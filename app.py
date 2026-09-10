925153a2d7242e5e5556a6ccc2591faa6b9d038e
Anirban Pal <anirbanpal022@gmail.com>
2026-09-10 15:35:07 +0530

    push_by_me

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ from backend.app.core.logging_config import logger
 app = create_app()
 
 if __name__ == "__main__":
-    logger.info(f"Starting AI Code Review Agent backend on port {config.PORT}...")
+    logger.info(f"Starting AI Code Review Agent backend on port... {config.PORT}...")
     app.run(host="0.0.0.0", port=config.PORT, debug=config.DEBUG)
